63e60946e9ecd656bb6e1d8f95a1cad8ef23053c
John Ide <ide.johnc@gmail.com>
2018-03-28 10:21:43 -0400

    Making post request for sessions

--- api/app.py
+++ api/app.py
@@ -1,19 +1,25 @@
 import uuid
 import json
+from pprint import pprint
 
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/session")
+@app.route("/session", methods=['GET', 'POST'])
 def generate_session():
-    data = {
-        "session_id": str(uuid.uuid4())
-    }
+    if request.method == 'GET':
+        data = {
+            "session_id": str(uuid.uuid4())
+        }
 
-    return json.dumps(data).encode()
+        return json.dumps(data).encode()
+    else:
+        pprint(request)
+        pprint(dir(request))
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
