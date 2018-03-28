5d99f10ea0ba5da9d68b0c0064a889b66177cd9d
John Ide <ide.johnc@gmail.com>
2018-03-28 11:12:06 -0400

    Receiving POST data

--- api/app.py
+++ api/app.py
@@ -8,18 +8,17 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/session", methods=['GET', 'POST'])
+@app.route("/session", methods=["GET", "POST"])
 def generate_session():
-    if request.method == 'GET':
+    if request.method == "GET":
         data = {
             "session_id": str(uuid.uuid4())
         }
 
-        return json.dumps(data).encode()
+        return json.dumps(data)
     else:
-        pprint(request)
-        pprint(dir(request))
-
+        data = request.data.decode()
+        return data
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
