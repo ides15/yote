6696df9f31a0e28a9e665f458bf86b26fca0cf79
John Ide <ide.johnc@gmail.com>
2018-03-27 23:24:08 -0400

    Inital CLI creation

---
+++ cli/app.py
@@ -0,0 +1,28 @@
+import sys
+import os
+import json
+
+import fire
+
+cwd = os.getcwd()
+yote_dir_path = cwd + "/.yote"
+yote_config_file = yote_dir_path + "/config"
+
+def init(url):
+    if os.path.exists(yote_dir_path):
+        print('Yote session already initialized')
+    else:
+        os.mkdir(yote_dir_path)
+        print('Yote session initialized')
+    
+    with open(yote_config_file, "wb") as file:
+        data = {
+            "session_id": url
+        }
+
+        file.write(json.dumps(data).encode())
+
+if __name__ == "__main__":
+    fire.Fire({
+        "init": init
+    })
