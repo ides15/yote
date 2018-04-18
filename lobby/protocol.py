357de2d7385b13541ef40f5f479a426ab25c1eb4
John Ide <ide.johnc@gmail.com>
2018-04-18 01:27:50 -0400

    Protocol that is the direct websocket interface

---
+++ lobby/protocol.py
@@ -0,0 +1,15 @@
+from autobahn.asyncio.websocket import WebSocketServerProtocol
+
+
+class YoteServerProtocol(WebSocketServerProtocol):
+    def onOpen(self):
+        self.factory.register(self)
+
+    def onMessage(self, payload, isBinary):
+        if not isBinary:
+            msg = "{} from {}".format(payload.decode('utf8'), self.peer)
+            self.factory.broadcast(msg)
+
+    def connectionLost(self, reason):
+        WebSocketServerProtocol.connectionLost(self, reason)
+        self.factory.unregister(self)
