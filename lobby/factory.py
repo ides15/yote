5932a103e2f07a0019b6130783fce0f0bfba0659
John Ide <ide.johnc@gmail.com>
2018-04-18 01:27:29 -0400

    Factory that runs the main server protocol

---
+++ lobby/factory.py
@@ -0,0 +1,23 @@
+from autobahn.asyncio.websocket import WebSocketServerFactory
+
+
+class YoteServerFactory(WebSocketServerFactory):
+    def __init__(self, url):
+        WebSocketServerFactory.__init__(self, url)
+        self.clients = []
+
+    def register(self, client):
+        if client not in self.clients:
+            print("registered client {}".format(client.peer))
+            self.clients.append(client)
+
+    def unregister(self, client):
+        if client in self.clients:
+            print("unregistered client {}".format(client.peer))
+            self.clients.remove(client)
+
+    def broadcast(self, msg):
+        print("broadcasting message '{}' ..".format(msg))
+        for c in self.clients:
+            c.sendMessage(msg.encode('utf8'))
+            print("message sent to {}".format(c.peer))
