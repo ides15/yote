e443ed45aa871d61ea1c054ed80afdf66a6a5313
John Ide <ide.johnc@gmail.com>
2018-04-23 23:36:40 -0400

    Adding connections to the connections list on socket connections and disconnections

--- frontend/js/socket.js
+++ frontend/js/socket.js
@@ -1,12 +1,7 @@
 var server = new WebSocket('ws://localhost:10000');
 
-server.onclose = function () {
-    $('#status').text("Socket is closed");
-    $('#ready_state').text("Ready state: " + server.readyState)
-}
-
-server.onerror = function () {
-    $('#status').text("Socket has error");
+server.onopen = function () {
+    $('#status').text("Socket is open");
     $('#ready_state').text("Ready state: " + server.readyState)
 }
 
@@ -14,6 +9,21 @@ server.onmessage = function (e) {
     $('#ready_state').text("Ready state: " + server.readyState);
 
     var res = JSON.parse(e.data);
+    // console.log(res);
+
+    if (res.connections) {
+        console.log("connections message");
+
+        var connections_list = $('#connections_list');
+        connections_list.empty();
+
+        $.each(res.connections, function (index, element) {
+            var li = $('<li class="connection"/>').text(element);
+            li.appendTo(connections_list);
+        });
+
+        return;
+    }
 
     var replacement = res.text;
     var from = res.from;
@@ -25,9 +35,16 @@ server.onmessage = function (e) {
     } else {
         editor.getDoc().replaceRange(replacement, from, to, origin);
     }
+
+    editor.indentLine(to.line, "smart");
 }
 
-server.onopen = function () {
-    $('#status').text("Socket is open");
+server.onerror = function () {
+    $('#status').text("Socket has error");
+    $('#ready_state').text("Ready state: " + server.readyState)
+}
+
+server.onclose = function () {
+    $('#status').text("Socket is closed");
     $('#ready_state').text("Ready state: " + server.readyState)
 }
